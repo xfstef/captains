f8dff74be2eff84013317fcbd79eff4645290975
Stefan Padureanu <stefan.padureanu@force24.co.uk>
2020-05-14 22:46:19 +0300

    Added more objects to the map and loaded them on start.

--- AdventureMap.gd
+++ AdventureMap.gd
@@ -6,13 +6,16 @@ extends Node2D
 var mapPath = "res://Maps/test1.json"
 
 var mapGroundMatrix = []
+var mapPropsMatrix = []
 var mapWidth = 0
 var mapHeight = 0
 var groundTileMap
+var propsTileMap
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	groundTileMap = get_node("TM-Ground")
+	propsTileMap = get_node("TM-Props")
 	loadMapData()
 	
 func loadMapData():
@@ -30,10 +33,15 @@ func loadMapData():
 	for y in range(mapHeight):
 		mapGroundMatrix.append([])
 		mapGroundMatrix[y] = []
+		mapPropsMatrix.append([])
+		mapPropsMatrix[y] = []
 		for x in range(mapWidth):
 			mapGroundMatrix[y].append([])
 			mapGroundMatrix[y][x] = payload.tiles[y][x][0]
 			groundTileMap.set_cell(x, y, mapGroundMatrix[y][x])
+			mapPropsMatrix[y].append([])
+			mapPropsMatrix[y][x] = payload.tiles[y][x][1]
+			propsTileMap.set_cell(x, y, mapPropsMatrix[y][x])
 	
 	file.close()
 
